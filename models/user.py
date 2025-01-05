f32f5af314c68284a38c106ac865cf495e1b71f9
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-05 14:41:12 -0800

    Implement DBStorage for Users.

--- models/user.py
+++ models/user.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-import models
-import sqlalchemy
+""" User Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        # Establishes relationship between User and Place models
-        places = relationship("Place", backref="user")
-        # Establishes relationship between User and Review models
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """This is the class for user"""
+    __tablename__ = 'users'
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", back_populates="user", cascade="all, delete-orphan")
