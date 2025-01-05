824c3ab3dc778858363443a6e59f9fc7d2a67da5
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-05 11:59:46 -0800

    Implement DBStorage for Users.

--- models/user.py
+++ models/user.py
@@ -2,16 +2,18 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    # Required fields that can't be null
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-
-    # Optional fields that can be null
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        """Initializes User"""
+        super().__init__(*args, **kwargs)
