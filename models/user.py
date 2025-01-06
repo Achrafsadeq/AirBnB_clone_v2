0d9fa53ececa8445402260555d44695052af1142
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-05 17:46:49 -0800

    update

--- models/user.py
+++ models/user.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-""" User Module for HBNB project """
+"""User module for the AirBnB clone project."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """This is the class for user"""
+    """User class for representing users in the AirBnB clone.
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
     __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    places = relationship("Place", back_populates="user", cascade="all, delete-orphan")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
